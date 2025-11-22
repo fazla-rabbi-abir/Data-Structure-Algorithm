e518c76ae946c507ab655615cde02ffa51128378
Md Fazla Rabbi Abir <131839058+fazla-rabbi-abir@users.noreply.github.com>
2025-11-22 19:18:42 +0600

    Add files via upload

---
+++ A_Sort_Insertion.cpp
@@ -0,0 +1,18 @@
+void insertion_sort(int A[], int n)
+{
+    int i, j, temp;
+
+    for(i = 1; i < n; i++)
+    {
+        temp = A[i];
+        j = i - 1;
+
+        while(j >= 0 && A[j] > temp)
+        {
+            A[j + 1] = A[j];
+            j--;
+        }
+
+        A[j + 1] = temp;
+    }
+}
